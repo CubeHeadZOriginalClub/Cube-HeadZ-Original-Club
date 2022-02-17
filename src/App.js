4a8aa0985fab74a2a7e19eeba31f5bb8aaa44a32
CubeHeadZOriginalClub <99505698+CubeHeadZOriginalClub@users.noreply.github.com>
2022-02-17 19:16:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -249,13 +249,13 @@ function App() {
             >
               <StyledButton
                 onClick={(e) => {
-                  window.open("/config/roadmap.pdf", "_blank");
+                  window.open("/config/Discord.pdf", "_blank");
                 }}
                 style={{
                   margin: "5px",
                 }}
               >
-                Roadmap
+                Discord
               </StyledButton>
               <StyledButton
                 style={{
